eaf859fbc45ddecec80d971872f874be1c2c15d9
2hyunjinn <huenjin04@naver.com>
2025-01-08 23:36:10 +0900

    [chore] #21 WeekDate 수정

--- src/main/java/com/ggang/be/domain/constant/WeekDate.java
+++ src/main/java/com/ggang/be/domain/constant/WeekDate.java
@@ -1,11 +1,31 @@
 package com.ggang.be.domain.constant;
 
+import java.time.DayOfWeek;
+
 public enum WeekDate {
-    MON,
-    TUE,
-    WED,
-    THU,
-    FRI,
-    SAT,
-    SUN;
+    MON("Monday"),
+    TUE("Tuesday"),
+    WED("Wednesday"),
+    THU("Thursday"),
+    FRI("Friday"),
+    SAT("Saturday"),
+    SUN("Sunday");
+
+    private final String fullName;
+
+    WeekDate(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public static WeekDate fromDayOfWeek(DayOfWeek dayOfWeek) {
+        return switch (dayOfWeek) {
+            case MONDAY -> MON;
+            case TUESDAY -> TUE;
+            case WEDNESDAY -> WED;
+            case THURSDAY -> THU;
+            case FRIDAY -> FRI;
+            case SATURDAY -> SAT;
+            case SUNDAY -> SUN;
+        };
+    }
 }

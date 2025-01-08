2550dc7d6e00f40be48342d4ee62452a43889f75
2hyunjinn <huenjin04@naver.com>
2025-01-08 16:22:05 +0900

    [fix] #16 ApiResponse record로 변경

--- src/main/java/com/ggang/be/api/common/ApiResponse.java
+++ src/main/java/com/ggang/be/api/common/ApiResponse.java
@@ -3,12 +3,7 @@ package com.ggang.be.api.common;
 import lombok.Getter;
 
 @Getter
-public class  ApiResponse<T> {
-    private final boolean success;
-    private final int code;
-    private final String message;
-    private final T data;
-
+public record ApiResponse<T>(boolean success, int code, String message, T data) {
     public static <T> ApiResponse<T> success(ResponseSuccess responseSuccess, T data) {
         return new ApiResponse<>(true, responseSuccess.getCode(), responseSuccess.getMessage(), data);
     }
@@ -17,14 +12,8 @@ public class  ApiResponse<T> {
         return new ApiResponse<>(true, responseSuccess.getCode(), responseSuccess.getMessage(), null);
     }
 
-    public static <T> ApiResponse <T> error(ResponseError responseError) {
+    public static <T> ApiResponse<T> error(ResponseError responseError) {
         return new ApiResponse<>(false, responseError.getCode(), responseError.getMessage(), null);
     }
 
-    public ApiResponse(boolean success, int code, String message, T data) {
-        this.success = success;
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
 }

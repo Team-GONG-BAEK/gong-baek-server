616b6d52fa1f15d16dc9c8f61e7782f4677f04ca
Khyojun <khyojun1104@gmail.com>
2025-01-08 23:35:50 +0900

    [fix] #25 compile이 안되게 만든 getter제거

--- src/main/java/com/ggang/be/api/common/ApiResponse.java
+++ src/main/java/com/ggang/be/api/common/ApiResponse.java
@@ -2,7 +2,6 @@ package com.ggang.be.api.common;
 
 import lombok.Getter;
 
-@Getter
 public record ApiResponse<T>(boolean success, int code, String message, T data) {
     public static <T> ApiResponse<T> success(ResponseSuccess responseSuccess, T data) {
         return new ApiResponse<>(true, responseSuccess.getCode(), responseSuccess.getMessage(), data);

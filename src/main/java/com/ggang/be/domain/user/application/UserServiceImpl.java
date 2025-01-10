6e6e76f6659c2d1e4a35d3433f641c13bc518c78
Khyojun <khyojun1104@gmail.com>
2025-01-10 18:13:25 +0900

    [feat] #31 transactional 추가

--- src/main/java/com/ggang/be/domain/user/application/UserServiceImpl.java
+++ src/main/java/com/ggang/be/domain/user/application/UserServiceImpl.java
@@ -7,10 +7,12 @@ import com.ggang.be.domain.user.infra.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;

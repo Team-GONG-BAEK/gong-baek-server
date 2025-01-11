1bcc29b4bd36dfe752e0c80a599255ded08725fd
Khyojun <khyojun1104@gmail.com>
2025-01-11 17:13:53 +0900

    [test] #30 회원가입에 필요한 서비스 테스트 코드 작성

---
+++ src/test/java/com/ggang/be/domain/lectureTimeSlot/application/LectureTimeSlotServiceImplTest.java
@@ -0,0 +1,67 @@
+package com.ggang.be.domain.lectureTimeSlot.application;
+
+
+import static org.mockito.Mockito.times;
+
+import com.ggang.be.domain.constant.Gender;
+import com.ggang.be.domain.constant.Mbti;
+import com.ggang.be.domain.constant.WeekDate;
+import com.ggang.be.domain.lectureTimeSlot.LectureTimeSlotEntity;
+import com.ggang.be.domain.lectureTimeSlot.vo.LectureTimeSlotVo;
+import com.ggang.be.domain.lectureTimeSlot.infra.LectureTimeSlotRepository;
+import com.ggang.be.domain.school.SchoolEntity;
+import com.ggang.be.domain.user.UserEntity;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class LectureTimeSlotServiceImplTest {
+
+    @Mock
+    private LectureTimeSlotRepository lectureTimeSlotRepository;
+
+    @InjectMocks
+    private LectureTimeSlotServiceImpl lectureTimeSlotServiceImpl;
+
+
+    @Test
+    void saveLectureTimeSlot() {
+
+        //given
+        UserEntity testCaseUser = UserEntity.builder()
+            .school(SchoolEntity.builder().build())
+            .schoolMajorName("Computer Science")
+            .profileImg(1)
+            .nickname("TestUser")
+            .schoolGrade(3)
+            .enterYear(2020)
+            .mbti(Mbti.INFJ)
+            .gender(Gender.MAN)
+            .introduction("Hello, I am a test user.")
+            .build();
+
+        List<LectureTimeSlotVo> lectureTimeSlotVos = List.of(
+            new LectureTimeSlotVo(WeekDate.MON, 9.0, 10.5),
+            new LectureTimeSlotVo(WeekDate.WED, 14.0, 15.5),
+            new LectureTimeSlotVo(WeekDate.FRI, 16.0, 17.0)
+        );
+
+        List<LectureTimeSlotEntity> list = lectureTimeSlotVos.stream()
+            .map(lectureTimeSlotVo -> LectureTimeSlotVo.toEntity(lectureTimeSlotVo, testCaseUser))
+            .toList();
+
+        //when
+        lectureTimeSlotServiceImpl.saveLectureTimeSlot(lectureTimeSlotVos, testCaseUser);
+
+        //then
+
+        Mockito.verify(lectureTimeSlotRepository, times(3)).save(ArgumentMatchers.any(LectureTimeSlotEntity.class));
+
+    }
+}
--- src/test/java/com/ggang/be/domain/user/application/UserServiceImplTest.java
+++ src/test/java/com/ggang/be/domain/user/application/UserServiceImplTest.java
@@ -1,14 +1,22 @@
 package com.ggang.be.domain.user.application;
 
+import static org.hamcrest.Matchers.any;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 import com.ggang.be.api.exception.GongBaekException;
+import com.ggang.be.domain.constant.Gender;
+import com.ggang.be.domain.constant.Mbti;
+import com.ggang.be.domain.school.SchoolEntity;
+import com.ggang.be.domain.user.UserEntity;
+import com.ggang.be.domain.user.dto.SaveUserSignUp;
 import com.ggang.be.domain.user.infra.UserRepository;
+import jakarta.validation.constraints.Max;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -51,4 +59,45 @@ class UserServiceImplTest {
              .isInstanceOf(GongBaekException.class)
              .hasMessageContaining("이미 존재하는 닉네임입니다.");
      }
+
+
+    @Test
+    void saveUserBySignup_shouldSaveAndReturnUserEntity() {
+        // Given
+
+        SchoolEntity school = SchoolEntity.builder().schoolName("school").build();
+
+        SaveUserSignUp request = new SaveUserSignUp(
+            1,
+            "nickname",
+            Mbti.INFJ, // schoolGrade
+            "computer science",
+            4,
+            2024,
+            "hello",
+            Gender.MAN,
+            school
+        );
+
+        UserEntity expectedUserEntity = UserEntity.builder()
+            .nickname("nickname")
+            .school(school)
+            .schoolGrade(3)
+            .gender(Gender.MAN)
+            .introduction("introduction")
+            .mbti(Mbti.INFJ)
+            .profileImg(1)
+            .enterYear(2020)
+            .schoolMajorName("Computer Science")
+            .build();
+
+        when(userRepository.save(ArgumentMatchers.any(UserEntity.class))).thenReturn(expectedUserEntity);
+
+        // When
+        UserEntity result = userServiceImpl.saveUserBySignup(request);
+
+        // Then
+        Assertions.assertThat(result).isEqualTo(expectedUserEntity);
+    }
+
 }

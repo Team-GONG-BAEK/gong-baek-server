d0fb5f3774117450a76d6db460fe13ecf0c48adc
2hyunjinn <huenjin04@naver.com>
2025-01-11 18:02:22 +0900

    [feat] #35 dto 생성

---
+++ src/main/java/com/ggang/be/api/group/dto/GroupUserInfoResponseDto.java
@@ -0,0 +1,28 @@
+package com.ggang.be.api.group.dto;
+
+import com.ggang.be.domain.user.dto.UserInfo;
+
+public record GroupUserInfoResponseDto(
+        int profileImg,
+        String nickname,
+        String sex,
+        String schoolMajor,
+        int enterYear,
+        int schoolGrade,
+        String mbti,
+        String introduction
+) {
+    public static GroupUserInfoResponseDto of(UserInfo userInfo){
+        return new GroupUserInfoResponseDto(
+                userInfo.profileImg(),
+                userInfo.nickname(),
+                userInfo.sex(),
+                userInfo.schoolMajor(),
+                userInfo.enterYear(),
+                userInfo.schoolGrade(),
+                userInfo.mbti(),
+                userInfo.introduction()
+        );
+    }
+}
+

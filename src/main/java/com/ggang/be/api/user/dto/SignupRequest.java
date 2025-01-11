dff9faf41b9dace3234fac0fb10ab95aaa6455bd
Khyojun <khyojun1104@gmail.com>
2025-01-11 17:16:39 +0900

    [feat] #30 dto 클래스 작성

---
+++ src/main/java/com/ggang/be/api/user/dto/SignupRequest.java
@@ -0,0 +1,36 @@
+package com.ggang.be.api.user.dto;
+
+import com.ggang.be.api.user.vo.TimeTableVo;
+import com.ggang.be.domain.constant.Gender;
+import com.ggang.be.domain.constant.Mbti;
+import com.ggang.be.domain.school.SchoolEntity;
+import com.ggang.be.domain.school.application.School;
+import com.ggang.be.domain.user.dto.SaveUserSignUp;
+import java.util.List;
+
+public record SignupRequest(Integer profileImg,
+                            String nickname,
+                            Mbti mbti,
+                            String schoolName,
+                            String schoolMajor,
+                            Integer schoolGrade,
+                            Integer enterYear,
+                            String introduction,
+                            Gender sex,
+                            List<TimeTableVo> timeTable
+                            ) {
+
+    public static SaveUserSignUp toSaveUserSignUp(SignupRequest request, SchoolEntity school) {
+        return new SaveUserSignUp(
+                request.profileImg(),
+                request.nickname(),
+                request.mbti(),
+                request.schoolMajor(),
+                request.schoolGrade(),
+                request.enterYear(),
+                request.introduction(),
+                request.sex(),
+                school
+        );
+    }
+}

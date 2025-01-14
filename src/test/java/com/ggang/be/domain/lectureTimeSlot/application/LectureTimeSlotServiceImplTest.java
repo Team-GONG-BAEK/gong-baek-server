cb764689f8f297751df43f44ff6f1a32eeb99f2f
Khyojun <khyojun1104@gmail.com>
2025-01-15 02:55:05 +0900

    [test] #48 내 강의 시간표 조회를 위한 service 코드 테스트 코드 작성 진행

--- src/test/java/com/ggang/be/domain/lectureTimeSlot/application/LectureTimeSlotServiceImplTest.java
+++ src/test/java/com/ggang/be/domain/lectureTimeSlot/application/LectureTimeSlotServiceImplTest.java
@@ -2,17 +2,22 @@ package com.ggang.be.domain.lectureTimeSlot.application;
 
 
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 import com.ggang.be.domain.constant.Gender;
 import com.ggang.be.domain.constant.Mbti;
 import com.ggang.be.domain.constant.WeekDate;
+import com.ggang.be.domain.lectureTimeSlot.fixture.LectureTimeSlotFixture;
 import com.ggang.be.domain.timslot.lectureTimeSlot.LectureTimeSlotEntity;
 import com.ggang.be.domain.timslot.lectureTimeSlot.application.LectureTimeSlotServiceImpl;
 import com.ggang.be.domain.timslot.lectureTimeSlot.vo.LectureTimeSlotVo;
 import com.ggang.be.domain.timslot.lectureTimeSlot.infra.LectureTimeSlotRepository;
 import com.ggang.be.domain.school.SchoolEntity;
 import com.ggang.be.domain.user.UserEntity;
+import com.ggang.be.domain.user.fixture.UserEntityFixture;
 import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -65,4 +70,35 @@ class LectureTimeSlotServiceImplTest {
         Mockito.verify(lectureTimeSlotRepository, times(3)).save(ArgumentMatchers.any(LectureTimeSlotEntity.class));
 
     }
+
+
+    @Test
+    @DisplayName("유저가 들어갈 수 없는 시간표를 조회한다.")
+    void readUserTime(){
+
+        //given
+        UserEntity userEntity = UserEntityFixture.create();
+
+        LectureTimeSlotEntity testCase1 = LectureTimeSlotFixture.makeLectureTimeSlotEntity();
+        LectureTimeSlotEntity testCase2 = LectureTimeSlotFixture.makeLectureTimeSlotEntity();
+        LectureTimeSlotEntity testCase3 = LectureTimeSlotFixture.makeLectureTimeSlotEntity();
+
+        when(lectureTimeSlotRepository.findAllByUserEntity(
+            userEntity)).thenReturn(List.of(
+            testCase1,
+            testCase2,
+            testCase3));
+        //when
+        List<LectureTimeSlotEntity> lectureTimeSlotEntities = lectureTimeSlotServiceImpl.readUserTime(
+            userEntity);
+
+        //then
+        Assertions.assertThat(lectureTimeSlotEntities).hasSize(3)
+            .containsExactlyInAnyOrder(
+                testCase1,
+                testCase2,
+                testCase3);
+
+    }
+
 }

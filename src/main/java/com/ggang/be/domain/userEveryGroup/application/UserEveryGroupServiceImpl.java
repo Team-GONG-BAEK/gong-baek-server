90b04198a14af69f4f27db7e88fd3496f6cea1ac
2hyunjinn <huenjin04@naver.com>
2025-01-15 01:47:44 +0900

    [chore] #50 파일명 변경

---
+++ src/main/java/com/ggang/be/domain/userEveryGroup/application/UserEveryGroupServiceImpl.java
@@ -0,0 +1,62 @@
+package com.ggang.be.domain.userEveryGroup.application;
+
+import com.ggang.be.api.userEveryGroup.service.UserEveryGroupService;
+import com.ggang.be.domain.group.GroupVoMaker;
+import com.ggang.be.domain.group.everyGroup.EveryGroupEntity;
+import com.ggang.be.domain.group.everyGroup.dto.ReadEveryGroup;
+import com.ggang.be.domain.user.UserEntity;
+import com.ggang.be.domain.userEveryGroup.UserEveryGroupEntity;
+import com.ggang.be.domain.userEveryGroup.dto.NearestEveryGroup;
+import com.ggang.be.domain.userEveryGroup.infra.UserEveryGroupRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class UserEveryGroupServiceImpl implements UserEveryGroupService {
+
+    private final UserEveryGroupRepository userEveryGroupRepository;
+    private final GroupVoMaker groupVoMaker;
+
+    @Override
+    public ReadEveryGroup getMyAppliedGroups(UserEntity currentUser, boolean status){
+        List<UserEveryGroupEntity> userEveryGroupEntities = getMyEveryGroup(currentUser);
+
+        return ReadEveryGroup.of(groupVoMaker.makeEveryGroup(getGroupsByStatus(userEveryGroupEntities, status)));
+    }
+
+    @Override
+    public NearestEveryGroup getMyNearestEveryGroup(UserEntity currentUser){
+        List<UserEveryGroupEntity> userEveryGroupEntities = getMyEveryGroup(currentUser);
+
+        EveryGroupEntity nearestGroup = getNearestGroup(getGroupsByStatus(userEveryGroupEntities, true));
+
+        return NearestEveryGroup.toDto(nearestGroup);
+    }
+
+    private EveryGroupEntity getNearestGroup(List<EveryGroupEntity> groups) {
+        return groups.stream()
+                .min(Comparator.comparing(EveryGroupEntity::getDueDate))
+                .orElse(null);
+    }
+
+    private List<UserEveryGroupEntity> getMyEveryGroup(UserEntity currentUser){
+        return userEveryGroupRepository.findByUserEntity_id(currentUser.getId());
+    }
+
+    private List<EveryGroupEntity> getGroupsByStatus(List<UserEveryGroupEntity> userEveryGroupEntities, boolean status) {
+        return userEveryGroupEntities.stream()
+                .map(UserEveryGroupEntity::getEveryGroupEntity)
+                .filter(group -> (status && group.getStatus().isActive()) || (!status && group.getStatus().isClosed()))
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/com/ggang/be/domain/userOnceGroup/application/UserOnceGroupServiceImpl.java
@@ -0,0 +1,62 @@
+package com.ggang.be.domain.userOnceGroup.application;
+
+import com.ggang.be.api.userOnceGroup.service.UserOnceGroupService;
+import com.ggang.be.domain.group.GroupVoMaker;
+import com.ggang.be.domain.group.onceGroup.OnceGroupEntity;
+import com.ggang.be.domain.group.onceGroup.dto.ReadOnceGroup;
+import com.ggang.be.domain.user.UserEntity;
+import com.ggang.be.domain.userOnceGroup.UserOnceGroupEntity;
+import com.ggang.be.domain.userOnceGroup.dto.NearestOnceGroup;
+import com.ggang.be.domain.userOnceGroup.infra.UserOnceGroupRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class UserOnceGroupServiceImpl implements UserOnceGroupService {
+
+    private final UserOnceGroupRepository userOnceGroupRepository;
+    private final GroupVoMaker groupVoMaker;
+
+    @Override
+    public ReadOnceGroup getMyAppliedGroups(UserEntity currentUser, boolean status){
+        List<UserOnceGroupEntity> userOnceGroupEntities = getMyOnceGroup(currentUser);
+
+        return ReadOnceGroup.of(groupVoMaker.makeOnceGroup(getGroupsByStatus(userOnceGroupEntities, status)));
+    }
+
+    @Override
+    public NearestOnceGroup getMyNearestOnceGroup(UserEntity currentUser){
+        List<UserOnceGroupEntity> userOnceGroupEntities = getMyOnceGroup(currentUser);
+
+        OnceGroupEntity nearestGroup = getNearestGroup(getGroupsByStatus(userOnceGroupEntities, true));
+
+        return NearestOnceGroup.toDto(nearestGroup);
+    }
+
+    private OnceGroupEntity getNearestGroup(List<OnceGroupEntity> groups) {
+        return groups.stream()
+                .min(Comparator.comparing(OnceGroupEntity::getGroupDate))
+                .orElse(null);
+    }
+
+    private List<UserOnceGroupEntity> getMyOnceGroup(UserEntity currentUser){
+        return userOnceGroupRepository.findByUserEntity_id(currentUser.getId());
+    }
+
+    private List<OnceGroupEntity> getGroupsByStatus(List<UserOnceGroupEntity> userOnceGroupEntities, boolean status) {
+        return userOnceGroupEntities.stream()
+                .map(UserOnceGroupEntity::getOnceGroupEntity)
+                .filter(group -> (status && group.getStatus().isActive()) || (!status && group.getStatus().isClosed()))
+                .collect(Collectors.toList());
+    }
+
+}

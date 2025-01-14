5e39a479fd44823555d381858b57ce0702ad294a
2hyunjinn <huenjin04@naver.com>
2025-01-14 17:39:46 +0900

    [chore] #34 폴더 변경

--- src/main/java/com/ggang/be/domain/everyGroup/infra/EveryGroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ggang.be.domain.everyGroup.infra;
-
-import com.ggang.be.domain.everyGroup.EveryGroupEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EveryGroupRepository extends JpaRepository<EveryGroupEntity, Long> {
-
-}
--- src/main/java/com/ggang/be/domain/onceGroup/infra/OnceGroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ggang.be.domain.onceGroup.infra;
-
-import com.ggang.be.domain.onceGroup.OnceGroupEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OnceGroupRepository extends JpaRepository<OnceGroupEntity, Long> {
-
-}

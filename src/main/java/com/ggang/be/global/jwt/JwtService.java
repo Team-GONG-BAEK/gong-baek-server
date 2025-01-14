277922aa3d1800dd456c6495ba4d64644989f57b
2hyunjinn <huenjin04@naver.com>
2025-01-14 17:40:37 +0900

    [chore] #34 JwtService 변경

--- src/main/java/com/ggang/be/global/jwt/JwtService.java
+++ src/main/java/com/ggang/be/global/jwt/JwtService.java
@@ -1,14 +1,17 @@
 package com.ggang.be.global.jwt;
 
+import com.ggang.be.api.common.ResponseError;
+import com.ggang.be.api.exception.GongBaekException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-import javax.crypto.SecretKey;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -18,35 +21,63 @@ public class JwtService {
     private static final String USER_ID = "userId";
     private final JwtProperties jwtProperties;
 
-    public String createAccessToken(final Long userId){
-        SecretKey secretKey = Keys.hmacShaKeyFor(jwtProperties.getKey().getBytes(StandardCharsets.UTF_8));
-        return  Jwts.builder()
-            .subject(userId.toString())
-            .expiration(new Date(System.currentTimeMillis() + jwtProperties.getAccessExpiration()))
-            .claim(USER_ID, userId)
-            .signWith(secretKey)
-            .compact();
+    public String createAccessToken(final Long userId) {
+        SecretKey secretKey = getSecretKey();
+        return Jwts.builder()
+                .subject(userId.toString())
+                .expiration(new Date(System.currentTimeMillis() + jwtProperties.getAccessExpiration()))
+                .claim(USER_ID, userId)
+                .signWith(secretKey)
+                .compact();
     }
 
-    public String createRefreshToken(final Long userId){
-        SecretKey secretKey = Keys.hmacShaKeyFor(jwtProperties.getKey().getBytes(StandardCharsets.UTF_8));
-        return  Jwts.builder()
-            .subject(userId.toString())
-            .expiration(new Date(System.currentTimeMillis() + jwtProperties.getRefreshExpiration()))
-            .claim(USER_ID, userId)
-            .signWith(secretKey)
-            .compact();
+    public String createRefreshToken(final Long userId) {
+        SecretKey secretKey = getSecretKey();
+        return Jwts.builder()
+                .subject(userId.toString())
+                .expiration(new Date(System.currentTimeMillis() + jwtProperties.getRefreshExpiration()))
+                .claim(USER_ID, userId)
+                .signWith(secretKey)
+                .compact();
     }
 
-    public String parseTokenAndGetUserId(String token){
+    public Long parseTokenAndGetUserId(String token) {
+        isValidToken(token);
+
         try {
-            SecretKey secretKey = Keys.hmacShaKeyFor(jwtProperties.getKey().getBytes(StandardCharsets.UTF_8));
-            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get(USER_ID).toString();
-        }catch (JwtException e){
+            String splitToken = token.split(" ")[1];
+            SecretKey secretKey = getSecretKey();
+            return Long.parseLong(parseTokenAndGetUserId(secretKey, splitToken));
+        } catch (JwtException | NumberFormatException e) {
             log.error("JWT parsing error : {}", e.getMessage());
-            throw e;
+            throw new GongBaekException(ResponseError.INVALID_TOKEN);
+        }
+
+    }
+
+    private String parseTokenAndGetUserId(SecretKey secretKey, String splitToken) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(splitToken)
+                .getPayload().get(USER_ID).toString();
+    }
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(
+                jwtProperties.getKey().getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    public void isValidToken(String token) {
+        if (token == null || !token.startsWith("Bearer "))
+            throw new GongBaekException(ResponseError.INVALID_TOKEN);
+
+        try {
+            String splitToken = token.split(" ")[1];
+            SecretKey secretKey = getSecretKey();
+
+            Long.parseLong(parseTokenAndGetUserId(secretKey, splitToken));
+        } catch (JwtException | NumberFormatException e) {
+            log.error("JWT parsing error : {}", e.getMessage());
+            throw new GongBaekException(ResponseError.INVALID_TOKEN);
         }
     }
-
-
 }

1ead3d0c0a14c435f4a54b440d29fc7d7935122e
Khyojun <khyojun1104@gmail.com>
2025-01-15 18:18:04 +0900

    [test] #62 jwtService reIssue 관련 테스트 코드 작성

--- src/test/java/com/ggang/be/global/jwt/JwtServiceTest.java
+++ src/test/java/com/ggang/be/global/jwt/JwtServiceTest.java
@@ -1,5 +1,12 @@
 package com.ggang.be.global.jwt;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+import com.ggang.be.api.user.service.UserService;
+import com.ggang.be.domain.user.UserEntity;
+import com.ggang.be.domain.user.fixture.UserEntityFixture;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
@@ -10,9 +17,25 @@ import javax.crypto.SecretKey;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 
+@ExtendWith(MockitoExtension.class)
 public class JwtServiceTest {
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private JwtProperties jwtProperties;
+
+    @Spy
+    @InjectMocks
+    private JwtService jwtService;
 
     @Test
     @DisplayName("jwt 토큰 생성 및 파싱 테스트")
@@ -38,6 +61,40 @@ public class JwtServiceTest {
         Assertions.assertThat(expiration).isAfter(new Date());
         Assertions.assertThat(Long.parseLong(parsedUserId)).isEqualTo(userId);
         }
+
+    @Test
+    void reIssueToken() throws InterruptedException {
+        //given
+        String secret = "abcdefghijklmnopqrstuvwxyz123456abcdefghijklmnopqrstuvwxyz123456";
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        Long userId = 1L;
+        UserEntity fixture = UserEntityFixture.create();
+
+        Date date = new Date(System.currentTimeMillis() + 86400000000L);
+        String token = Jwts.builder()
+            .subject(userId.toString())
+            .expiration(date)
+            .claim("userId", userId)
+            .signWith(secretKey)
+            .compact();
+
+        when(jwtProperties.getKey()).thenReturn(secret);
+        when(userService.getUserById(userId)).thenReturn(fixture);
+        doNothing().when(userService).validateRefreshToken(fixture, token);
+
+       String accessToken = jwtService.createAccessToken(userId);
+        String refreshToken = jwtService.createRefreshToken(userId);
+        String addBearerPrefix = "Bearer " + token;
+
+        Thread.sleep(1000);
+
+        //when
+        TokenVo tokenVo = jwtService.reIssueToken(addBearerPrefix);
+
+        // then
+       Assertions.assertThat(tokenVo.accessToken()).isNotEqualTo(accessToken);
+        Assertions.assertThat(tokenVo.refreshToken()).isNotEqualTo(refreshToken);
     }
+}
 
 

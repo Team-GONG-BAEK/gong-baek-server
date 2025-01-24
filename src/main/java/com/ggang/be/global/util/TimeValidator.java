24818a4ff6c760d2eceef90c74a0910eca6a714f
Khyojun <khyojun1104@gmail.com>
2025-01-24 21:49:23 +0900

    [hotfix] #185 enterYear, timeTable duplicate 검증 진행

--- src/main/java/com/ggang/be/global/util/TimeValidator.java
+++ src/main/java/com/ggang/be/global/util/TimeValidator.java
@@ -2,7 +2,9 @@ package com.ggang.be.global.util;
 
 import com.ggang.be.api.common.ResponseError;
 import com.ggang.be.api.exception.GongBaekException;
+import com.ggang.be.api.user.vo.TimeTableVo;
 import com.ggang.be.domain.constant.WeekDay;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
@@ -28,6 +30,13 @@ public class TimeValidator {
         }
     }
 
+    public static void isYearAfterNow(int year){
+        if(year > LocalDate.now().getYear()) {
+            log.error("year {} is after now {}", year, LocalDate.now().getYear());
+            throw new GongBaekException(ResponseError.BAD_REQUEST);
+        }
+    }
+
     public static void isDateBeforeNow(LocalDate writeDate){
         if(writeDate.isBefore(LocalDate.now())) {
             log.error("writeDate {} is before now {}", writeDate, LocalDate.now());
@@ -38,6 +47,16 @@ public class TimeValidator {
     public static void isSameDate(LocalDate date, LocalDate otherDate) {
         if(!date.equals(otherDate)) {
             log.error("localDate {} and otherDate {} is not same", date, otherDate);
+            throw new GongBaekException(ResponseError.BAD_REQUEST);
+        }
+    }
+
+    public static void hasDuplicateInfo(List<TimeTableVo> timeTableVos) {
+        long size = timeTableVos.size();
+        long count = timeTableVos.stream().distinct().count();
+
+        if(size != count){
+            log.error("timeTableVo has duplicate info");
             throw new GongBaekException(ResponseError.BAD_REQUEST);
         }
     }

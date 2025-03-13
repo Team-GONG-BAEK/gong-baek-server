7f2c466297256dbdc288252439dff49666f6ee81
2hyunjinn <huenjin04@naver.com>
2025-03-13 19:43:01 +0900

    [chore] #13 불필요한 주석 삭제

--- src/main/java/com/ggang/be/api/comment/strategy/EveryGroupCommentStrategy.java
+++ src/main/java/com/ggang/be/api/comment/strategy/EveryGroupCommentStrategy.java
@@ -42,7 +42,6 @@ public class EveryGroupCommentStrategy implements CommentStrategy {
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
 
-        // everyGroup.getComments().remove(comment);
         everyGroupService.deleteComment(findUserEntity, comment, dto.groupId());
     }
     @Override

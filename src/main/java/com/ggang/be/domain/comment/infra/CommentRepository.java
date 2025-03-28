ab75ed029e29934b4979bf83c6db1dce6a79a79d
2hyunjinn <huenjin04@naver.com>
2025-03-28 19:18:53 +0900

    [feat] #21 회원탈퇴 시 작성한 댓글 작성자 정보 삭제

--- src/main/java/com/ggang/be/domain/comment/infra/CommentRepository.java
+++ src/main/java/com/ggang/be/domain/comment/infra/CommentRepository.java
@@ -3,6 +3,8 @@ package com.ggang.be.domain.comment.infra;
 import com.ggang.be.domain.comment.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+import java.util.List;
 
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    List<CommentEntity> findAllByUserEntity_Id(Long userId);
 }

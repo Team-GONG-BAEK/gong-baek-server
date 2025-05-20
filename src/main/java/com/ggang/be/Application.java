8bd821d26841dbc9658528ca56931d4cc405c605
2hyunjinn <huenjin04@naver.com>
2025-05-20 20:33:06 +0900

    [feat] #62 특정 유저의 테스트용 이메일 추가

--- src/main/java/com/ggang/be/Application.java
+++ src/main/java/com/ggang/be/Application.java
@@ -2,6 +2,7 @@ package com.ggang.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 @EnableAsync
+@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {

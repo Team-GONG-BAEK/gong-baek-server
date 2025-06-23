ae2dde3bad49f5c7e2e8e342629d6b3d44da8ee6
Khyojun <khyojun1104@gmail.com>
2025-06-24 01:27:01 +0900

    [chore] #78 주석 삭제 진행

--- src/main/java/com/ggang/be/api/report/controller/ReportController.java
+++ src/main/java/com/ggang/be/api/report/controller/ReportController.java
@@ -23,13 +23,7 @@ public class ReportController {
 
 	private final ReportFacade reportFacade;
 	private final JwtService jwtService;
-
-	/**
-	 *  TODO : 이제 신고가 아닌 -> 신고를 통해 정지 block에 들어있는 유저라면 해당 신고 건을 보고 누가 누구한테 보여지면 안되는 사람인지 판별!
-	 *  TODO : block -> report -> reportId, reportedId 인지 확인하는 절차 필요!
-	 *
-	 */
-
+	
 	@PostMapping("/comment/{commentId}")
 	public ResponseEntity<ReportCommentResponse> reportComment(
 		@RequestHeader("Authorization") final String token,

b88a2395bf628efd1d0e740b220b773ca4e94091
2hyunjinn <huenjin04@naver.com>
2025-07-22 22:27:38 +0900

    [fix] #87 모임 신고 시 내가 신청한 모임이면 모임 신청 취소하기

--- src/main/java/com/ggang/be/api/facade/ReportFacade.java
+++ src/main/java/com/ggang/be/api/facade/ReportFacade.java
@@ -3,6 +3,7 @@ package com.ggang.be.api.facade;
 import com.ggang.be.api.comment.service.CommentService;
 import com.ggang.be.api.common.ResponseSuccess;
 import com.ggang.be.api.group.dto.GroupCreatorVo;
+import com.ggang.be.api.group.dto.GroupRequest;
 import com.ggang.be.api.group.facade.GroupFacade;
 import com.ggang.be.api.report.service.ReportService;
 import com.ggang.be.domain.block.application.BlockServiceImpl;
@@ -11,40 +12,39 @@ import com.ggang.be.domain.constant.GroupType;
 import com.ggang.be.domain.report.ReportEntity;
 import com.ggang.be.domain.user.UserEntity;
 import com.ggang.be.global.annotation.Facade;
-
 import lombok.RequiredArgsConstructor;
 
 @Facade
 @RequiredArgsConstructor
 public class ReportFacade {
 
-	private final ReportService reportService;
-	private final BlockServiceImpl blockService;
-	private final GroupFacade groupFacade;
-	private final CommentService commentService;
+    private final ReportService reportService;
+    private final BlockServiceImpl blockService;
+    private final GroupFacade groupFacade;
+    private final CommentService commentService;
 
-	public ResponseSuccess reportComment(long userId, long commentId) {
+    public ResponseSuccess reportComment(long userId, long commentId) {
 
-		CommentEntity commentEntity = commentService.findById(commentId);
+        CommentEntity commentEntity = commentService.findById(commentId);
 
-		UserEntity userEntity = commentEntity.getUserEntity();
-		long reportedId = userEntity.getId();
+        UserEntity userEntity = commentEntity.getUserEntity();
+        long reportedId = userEntity.getId();
 
-		ReportEntity reportEntity = reportService.reportComment(commentId, userId, reportedId);
+        ReportEntity reportEntity = reportService.reportComment(commentId, userId, reportedId);
 
-		blockService.blockUser(reportEntity, userEntity);
+        blockService.blockUser(reportEntity, userEntity);
 
-		return ResponseSuccess.CREATED;
-	}
+        return ResponseSuccess.CREATED;
+    }
 
 
-	public ResponseSuccess reportGroup(long userId, long groupId, GroupType groupType){
+    public ResponseSuccess reportGroup(long userId, long groupId, GroupType groupType) {
 
-		GroupCreatorVo groupCreator = groupFacade.findGroupCreator(groupType, groupId);
+        GroupCreatorVo groupCreator = groupFacade.findGroupCreator(groupType, groupId);
+        groupFacade.cancelMyApplication(userId, new GroupRequest(groupId, groupType));
+        reportService.reportGroup(groupId, userId, groupCreator.creatorId(), groupType);
 
-		reportService.reportGroup(groupId, userId, groupCreator.creatorId(), groupType);
-
-		return ResponseSuccess.CREATED;
-	}
+        return ResponseSuccess.CREATED;
+    }
 
 }
